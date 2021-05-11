b8ba61cd45507cfbc8a7ac306fb68971ec83ab59
igor-mircic <igor.mircic@live.com>
2021-05-11 16:04:53 +0200

    add local fonts

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 
   devtool: "inline-source-map",
@@ -33,6 +34,10 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
     ],
