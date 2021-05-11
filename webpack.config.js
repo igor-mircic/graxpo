d6a4fcf8bc3fe00144f4258476649af367612a03
igor-mircic <igor.mircic@live.com>
2021-05-11 13:46:33 +0200

    add webpack support for scss

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,32 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+
   module: {
     rules: [
       {
         test: /\.html$/,
         loader: "html-loader",
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
     ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
